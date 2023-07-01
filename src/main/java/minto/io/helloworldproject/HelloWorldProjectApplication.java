e0a80e7027a808bb89654b820f5fa3270f62c549
shinsunyoung <ajufresh@gmail.com>
2023-07-01 19:54:02 +0900

    init

---
+++ src/main/java/minto/io/helloworldproject/HelloWorldProjectApplication.java
@@ -0,0 +1,13 @@
+package minto.io.helloworldproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/minto/io/helloworldproject/HelloWorldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package minto.io.helloworldproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
